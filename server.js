28fe76f82a1411a99e998dec424f012ce6912b54
Anuj Kumar <kumar_191851@student.nitw.ac.in>
2022-11-16 09:00:27 +0530

    test

--- server.js
+++ server.js
@@ -2,26 +2,26 @@ const express = require("express");
 require("dotenv").config();
 const app = express();
 const router = express.Router();
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 const port = process.env.PORT || 5000;
-mongoose.connect(
-   process.env.MONGO_URL,
-   {
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-   },
-   () => {
-      console.log("[3] MongoDB Connected!");
-   }
-);
+// mongoose.connect(
+//    process.env.MONGO_URL,
+//    {
+//       useUnifiedTopology: true,
+//       useNewUrlParser: true,
+//       useCreateIndex: true,
+//       useFindAndModify: false,
+//    },
+//    () => {
+//       console.log("[3] MongoDB Connected!");
+//    }
+// );
 
 //middleware
 app.use(express.json());
 
 app.get("/", (req, res) => {
-   res.send("Welcome to Homepage...");
+   res.send("Welcome to App...");
 });
 
 app.listen(port, () => {
